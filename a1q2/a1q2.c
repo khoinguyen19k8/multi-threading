ec0fbf7412c8b773019e73fbd72637c5e17c4f49
akooi <khoinguyen19k8@gmail.com>
2022-01-26 01:32:48 +0100

    Complete a1q2

--- a1q2/a1q2.c
+++ a1q2/a1q2.c
@@ -72,6 +72,11 @@ void
 consume_enter(struct resource *resource)
 {
     /* XXXFILLEMIN */
+	pthread_mutex_lock(&resource -> mutex);
+	while ((resource->num_consumers) + 1 > resource->num_producers * resource->ratio)
+	{
+		pthread_cond_wait(&resource -> cond, &resource -> mutex);
+	}
     resource->num_consumers += 1;
 }
 
@@ -80,20 +85,29 @@ consume_exit(struct resource *resource)
 {
     /* XXXFILLEMIN */
     resource->num_consumers -= 1;
+	pthread_cond_signal(&resource -> cond);
+	pthread_mutex_unlock(&resource -> mutex);
 }
 
 void
 produce_enter(struct resource *resource)
 {
     /* XXXFILLEMIN */
-    resource->num_producers += 1;
+    pthread_mutex_lock(&resource -> mutex);
+	resource->num_producers += 1;
+    pthread_cond_signal(&resource -> cond);
 }
 
 void
 produce_exit(struct resource *resource)
 {
     /* XXXFILLMEIN */
+	while (resource->num_consumers > (resource->num_producers - 1) * resource->ratio)
+	{
+		pthread_cond_wait(&resource -> cond, &resource -> mutex);
+	}
     resource->num_producers -= 1;
+	pthread_mutex_unlock(&resource -> mutex);
 }
 
 /* Functions for the consumers and producers. */
